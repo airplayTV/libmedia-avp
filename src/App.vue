6e0b713d084ae0e5dbcee453c4c0bee05a51a5bc
example <dev@example.com>
2025-08-05 22:50:07 +0800

    更新播放器

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
+
+console.log('[github] https://github.com/airplayTV/libmedia-avp')
 </script>
 
 <template>
